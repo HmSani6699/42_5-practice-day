aabb0b49a72b0506439e11e85618e4ad09628459
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-13 13:01:34 +0600

    practice level two solve

--- js/level__2.js
+++ js/level__2.js
@@ -1,9 +1,16 @@
 let count = 0;
 const countNumber = () => {
-    document.getElementById('number').innerText = count;
     count++;
+    document.getElementById('number').innerText = count;
+    setCountNumberToLocalStorage(count)
+}
+const setCountNumberToLocalStorage = (number) => {
+    localStorage.setItem('number', number)
 }
 
-const getCountStored = () => {
-    const count = localStorage.getItem('count')
+const getCountNumber = () => {
+    const countNumber = localStorage.getItem('number');
+    document.getElementById('number').innerText = countNumber;
+    count = countNumber;
 }
+getCountNumber()
