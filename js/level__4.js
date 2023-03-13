38d8cb7f17c7f771f0ad592d17485b9fb7024aaa
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-13 17:54:11 +0600

    set and remove all info

--- js/level__4.js
+++ js/level__4.js
@@ -1,7 +1,52 @@
 // Get input value
-const getInputValue = (id) => {
+const inputFiledValue = (id) => {
     const getInputFiled = document.getElementById(id);
     const value = getInputFiled.value;
     getInputFiled.value = '';
     return value
+}
+
+//set item to localStorage
+const setKeyAndValueToLocalStorage = (key, value) => {
+    localStorage.setItem(key, value)
+}
+
+//Set name
+const addButton = () => {
+    const name = inputFiledValue('name-filed');
+    setKeyAndValueToLocalStorage('name', name)
+
+}
+
+//Delete name
+const deleteName = () => {
+    localStorage.removeItem('name')
+}
+
+//Set email
+const setEmail = () => {
+    const email = inputFiledValue('email-filed');
+    setKeyAndValueToLocalStorage('email', email)
+}
+
+//Delete email
+const deleteEmail = () => {
+    localStorage.removeItem('email')
+}
+
+//Set message
+const setMessage = () => {
+    const message = inputFiledValue('message-filed')
+    setKeyAndValueToLocalStorage('message', message)
+}
+
+//Delete message
+const deleteMessage = () => {
+    localStorage.removeItem('message')
+}
+
+
+// Resat all information in localStorage 
+const resatAllInformation = () => {
+    localStorage.clear()
 }
