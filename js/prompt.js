7455a50adcc8afa8ddce94cd482a5646ffe2fa7f
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-13 11:44:21 +0600

    practice proble two solve

--- js/prompt.js
+++ js/prompt.js
@@ -3,3 +3,11 @@ const promptBtn = () => {
     const promptValue = parseFloat(promptValueString);
     alert(isNaN(promptValue) ? 'please enter your number' : promptValue + 200)
 }
+
+// Confirm button
+const confirmBtn = () => {
+    const conFirmStringValue = confirm('Tomi ki tor website er url dekte sau')
+    if (conFirmStringValue === true) {
+        document.getElementById('url').innerText = location.href
+    }
+}
